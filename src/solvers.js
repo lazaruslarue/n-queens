0b1fddcf57389bcc68935bca90dcba1520df2d45
Hack Reactor <hackreactor@Hacks-Mac-mini.local>
2013-11-20 18:12:06 -0800

    tinker

--- src/solvers.js
+++ src/solvers.js
@@ -14,20 +14,59 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n){
   var solution = undefined; //fixme
-
+  var board = new Board({"n":n}); 
+  for (var i = 0; i < n; i++) {
+    board.togglePiece(i,i);
+  };
+  solution = board.rows();
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n){
   var solutionCount = undefined; //fixme
+  var Slate = function() { 
+    var rooks = 1;
+  };
+
+  Slate.prototype = new Board({"n":n});
+
+  Slate.prototype.valid = function () {
+    var toggleArray = [];
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        this.togglePiece(i,j);
+        if (!this.hasAnyRooksConflicts()){
+          toggleArray.push([i,j]);
+        }
+        this.togglePiece(i,j);
+      }
+    }
+    if (toggleArray.length === 0) {
+      return false;
+    } else if (toggleArray > 0 && (n > rooks)) {
+      return this.toggler(toggleArray);
+    }
+  }
+  Slate.prototype.toggler = function(toggleArray) {
+    this.rooks++;
+    for (var i = 0; i < toggleArray.length; i++) {
+      this.togglePiece(toggleArray[i]);
+      this.rooks++;
+      return this.valid();
+    }
+  };
+
+  Slate.prototype.togglePiece(0,0);
+  Slate.prototype.valid();
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
+
+
 
 
 
